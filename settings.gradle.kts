d5671840acd50206634a90169c710df0c04ced47
vbkuzmin <vovakuzmin@gmail.com>
2024-09-07 18:45:34 +0300

    feat(m1l1): start project and add first test

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion apply false
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "greedy-goblin"
+include("m1l1-first")
